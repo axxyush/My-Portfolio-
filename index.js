52faafb6ce2b2aafb5febdfe939e4215789b581d
Axxyush <ayushsrivastava192004@gmail.com>
2023-12-31 18:49:02 -0500

    added js

---
+++ index.js
@@ -0,0 +1,29 @@
+document.querySelector(".education").addEventListener("click", open_education)
+function open_education(){
+    window.location="education.html";
+}
+
+document.querySelector(".experience").addEventListener("click", open_experience)
+function open_experience(){
+    window.location="experience.html";
+}
+
+document.querySelector(".skills").addEventListener("click", open_skills)
+function open_skills(){
+    window.location="skills.html";
+}
+
+document.querySelector(".projects").addEventListener("click", open_projects)
+function open_projects(){
+    window.location="projects.html";
+}
+
+document.querySelector(".see-more").addEventListener("click", open_seemore)
+function open_seemore(){
+    window.location="seemore.html";
+}
+
+document.querySelector(".about-click").addEventListener("click", open_about)
+function open_about(){
+    window.location="about.html";
+}
