f8b2f0703012f47fda9f5b6835951b3a641cb69b
Axxyush <ayushsrivastava192004@gmail.com>
2024-01-01 00:35:43 -0500

    fixed js

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-document.querySelector(".education").addEventListener("click", open_education)
-function open_education(){
-    window.location="education.html";
-}
+document.addEventListener('DOMContentLoaded', function () {
 
-document.querySelector(".experience").addEventListener("click", open_experience)
-function open_experience(){
-    window.location="experience.html";
-}
+    document.querySelector(".about-click").addEventListener("click", open_about)
+    function open_about() {
+        window.location = "about.html";
+    }
 
-document.querySelector(".skills").addEventListener("click", open_skills)
-function open_skills(){
-    window.location="skills.html";
-}
+    document.querySelector(".name").addEventListener("click", open_name)
+    function open_name() {
+        window.location = "index.html";
+    }
 
-document.querySelector(".projects").addEventListener("click", open_projects)
-function open_projects(){
-    window.location="projects.html";
-}
+    document.querySelector(".education").addEventListener("click", open_education)
+    function open_education() {
+        window.location = "education.html";
+    }
 
-document.querySelector(".see-more").addEventListener("click", open_seemore)
-function open_seemore(){
-    window.location="seemore.html";
-}
+    document.querySelector(".experience").addEventListener("click", open_experience)
+    function open_experience() {
+        window.location = "experience.html";
+    }
 
-document.querySelector(".about-click").addEventListener("click", open_about)
-function open_about(){
-    window.location="about.html";
-}
+    document.querySelector(".skills").addEventListener("click", open_skills)
+    function open_skills() {
+        window.location = "skills.html";
+    }
+
+    document.querySelector(".projects").addEventListener("click", open_projects)
+    function open_projects() {
+        window.location = "projects.html";
+    }
+
+    document.querySelector(".see-more").addEventListener("click", open_seemore)
+    function open_seemore() {
+        window.location = "seemore.html";
+    }
+
+});
